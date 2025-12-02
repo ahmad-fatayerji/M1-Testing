a28d6e020dc58a71f7c097ad9553a895991a1d94
FATAYERJI Ahmad <ahmad.fatayerji@etu.univ-nantes.fr>
2025-12-02 02:14:54 +0100

    Mock tests implementation

--- src/test/java/fr/univnantes/basicplanner/impl/Exercice2Test.java
+++ src/test/java/fr/univnantes/basicplanner/impl/Exercice2Test.java
@@ -1,16 +1,78 @@
 package fr.univnantes.basicplanner.impl;
 
+import fr.univnantes.basicplanner.PlannerException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.configuration.IMockitoConfiguration;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class Exercice2Test {
 
-    @Test
-    void seeCurrentMeeting() {
+    //Setting up instants
+    Instant instantEleven;
+    Instant instantElevenThirty;
+    Instant instantTwelve;
+    Instant instantThirtheen;
+
+
+    //Setting up titles
+    String goodTitle;
+
+    //Setting up meetings
+    MeetingImpl meetingElevenToTwelve;
+
+
+    //Setting up planners
+    BasicPlannerImpl planner;
+
+    @BeforeEach
+    public void Setup(){
+
+        //Instantiating instants
+        instantEleven = LocalDateTime.of(2025,1,1,11,0).toInstant(ZoneOffset.UTC);
+        instantElevenThirty = LocalDateTime.of(2025,1,1,11,30).toInstant(ZoneOffset.UTC);
+        instantTwelve = LocalDateTime.of(2025,1,1,12,0).toInstant(ZoneOffset.UTC);
+        instantThirtheen = LocalDateTime.of(2025,1,1,13,0).toInstant(ZoneOffset.UTC);
+
+        //Instantiating titles
+        goodTitle = "workingTitle";
+
+        //Instantiating meetings
+        meetingElevenToTwelve = new MeetingImpl(instantEleven,instantTwelve,goodTitle);
+
     }
 
     @Test
-    void seeActiveReminders() {
+    @DisplayName("Test seeCurrentMeeting BasicPlannerImpl returns null when no upcoming meetings")
+    void TestBasicPlannerImplSeeCurrentMeetingNull() throws PlannerException {
+        //it's 13h, the only meeting we had was from 11h till 12h
+        SystemClock mockedClock = mock(SystemClock.class);
+        when(mockedClock.getCurrentTime()).thenReturn(instantThirtheen);
+
+        planner = new BasicPlannerImpl(mockedClock);
+        planner.addMeeting(meetingElevenToTwelve);
+
+        assertNull(planner.seeCurrentMeeting());
+    }
+
+    @Test
+    @DisplayName("Test seeCurrentMeeting BasicPlannerImpl is good when meeting now")
+    void TestBasicPlannerImplSeeCurrentMeetingGood() throws PlannerException {
+        SystemClock mockedClock = mock(SystemClock.class);
+        when(mockedClock.getCurrentTime()).thenReturn(instantElevenThirty);
+
+        planner = new BasicPlannerImpl(mockedClock);
+        planner.addMeeting(meetingElevenToTwelve);
+
+        assertEquals(meetingElevenToTwelve,planner.seeCurrentMeeting());
     }
 }
